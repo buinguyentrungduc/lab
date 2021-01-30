a2b334ffa40b84cb499f9e47ef476a422461a3b8
Duc <trungduclak@gmail.com>
2021-01-30 09:33:36 +0700

    hb5474gv6y4gvy6

--- 02-vonglap/javascript.js
+++ 02-vonglap/javascript.js
@@ -46,3 +46,82 @@ function mangLeTuN(a,b) {
     }
     return  ` mảng ${a} số lẻ bắt đầu từ ${b} là: `+  list;
 }
+function xuat10Chu(a){
+    var list = [];
+    for (let i = 0; i < 10; i++) {
+        list.push(a);
+    }
+    return list;
+}
+function tongLe(a, b) {
+    var sum = 0;
+    for (let i = a; i <= b; i++) {
+        if (i % 2 != 0) {
+            sum += i;
+        }
+    }
+    return ` Tổng lẻ từ ${a} đến ${b} là: ` + sum;
+}
+
+function mangLe(n) {
+    var list = [];
+    var i = 0;
+    while (list.length < n) {
+        if (i % 2 != 0) {
+            list.push(i);
+        }
+        i++;
+    }
+    return ` Mảng ${n} số lẻ nhỏ nhất là: ` + list;
+}
+
+function mangLeTuN(a, b) {
+    var c = b;
+    var list = [];
+    while (list.length < a) {
+        if (b % 2 != 0) {
+            list.push(b);
+        }
+        b++;
+    }
+    return ` Mảng ${a} số lẻ bắt đầu từ ${c} là: ` + list;
+}
+
+function KiemTraNT(n) {
+    var check = true;
+    if (n < 2) {
+        check = false;
+    }
+    else {
+        for (var i = 2; i < n - 1; i++) {
+            if (n % i == 0) {
+                check = false;
+                break;
+            }
+        }
+    }
+    return check;
+
+}
+
+function nNguyenTo(x) {
+    var list = [];
+    var n = 1 ;
+    while (list.length < x) {
+        if(KiemTraNT(n)== true){
+            list.push(n);
+        }n++;    
+    }
+    return ` Mảng ${x} số nguyên tố là: `+list;
+}
+
+function nNguyenToTuM(a,b) {
+    var list = [];
+    var n = b ;
+    while (list.length < a) {
+        if(KiemTraNT(n)== true){
+            list.push(n);
+        }n++;    
+    }
+    return list;
+}
